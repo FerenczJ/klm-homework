0e276b2b89b15c1b3fe55f072a5435f2fe79e2c6
mail.ferencz <mail.ferencz@gmail.com>
2024-02-18 17:49:12 +0100

    feature/KLM01 Removed unused import.

--- solution/itinerary/src/test/java/com/klm/itinerary/service/AirportServiceImplTest.java
+++ solution/itinerary/src/test/java/com/klm/itinerary/service/AirportServiceImplTest.java
@@ -6,9 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
